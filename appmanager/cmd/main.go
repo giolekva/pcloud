d5a58c4b4c668b2e2a9f587579d2ace54bdb8a4d
giolekva <giolekva@gmail.com>
2020-05-07 22:18:35 +0400

    appmanager: helm package unpacker

---
+++ appmanager/cmd/main.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	app "github.com/giolekva/pcloud/appmanager"
+
+	"github.com/golang/glog"
+)
+
+func main() {
+	unpacker := app.NewHelmUnpacker("/usr/local/bin/helm")
+	temps, err := unpacker.Unpack("/Users/lekva/dev/go/src/github.com/giolekva/pcloud/apps/rpuppy/chart",
+		"app-rpuppy",
+		"init-release",
+		map[string]string{
+			"replicas":    "2",
+			"servicePort": "8080",
+		},
+	)
+	if err != nil {
+		panic(err)
+	}
+	glog.Info(temps)
+}
