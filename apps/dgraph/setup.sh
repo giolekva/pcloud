37c5c546908d13d25a86984c15dd00e948d14dd7
giolekva <giolekva@gmail.com>
2020-12-06 18:19:59 +0400

    dgraph: use config file for whitelist flag

--- apps/dgraph/setup.sh
+++ apps/dgraph/setup.sh
@@ -1,15 +1,14 @@
-# kubectl create namespace dgraph
-# helm repo add dgraph https://charts.dgraph.io
-# dgraph/dgraph
-helm --namespace=dgraph install init /Users/lekva/dev/src/dgraph-charts/charts/dgraph \
+kubectl create namespace dgraph
+helm repo add dgraph https://charts.dgraph.io
+helm --namespace=dgraph install init dgraph/dgraph \
      --set fullnameOverride=dgraph \
-     --set image.repository=giolekva/dgraph-arm \
+     --set image.repository=dgraph/dgraph \
      --set image.tag=latest \
+     --set ratel.enabled=False \
      --set zero.replicaCount=1 \
      --set zero.persistence.size=1Gi \
      --set zero.persistence.storageClass=local-path \
      --set alpha.replicaCount=1 \
      --set alpha.persistence.size=1Gi \
      --set alpha.persistence.storageClass=local-path \
-     --set ratel.enabled=False \
-     --set alpha.whitelist=0.0.0.0:255.255.255.255
+     --set alpha.configFile."config\.yaml"="whitelist: '0.0.0.0:255.255.255.255'"
