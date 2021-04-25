51126ba9b92b97d95b04b3280f2f56a4b3bebc95
iomodo <wineson@gmail.com>
2021-04-25 23:04:00 +0400

    Add user store sql implementation

--- core/kg/model/errors.go
+++ core/kg/model/errors.go
@@ -7,4 +7,5 @@ var (
 	ErrInvalidInput = errors.New("invalid input")
 	ErrUnauthorized = errors.New("access denied")
 	ErrForbidden    = errors.New("forbidden")
+	ErrInternal     = errors.New("internal system error")
 )
--- core/kg/store/sqlstore/user_store.go
+++ core/kg/store/sqlstore/user_store.go
@@ -1,9 +1,14 @@
 package sqlstore
 
 import (
+	"database/sql"
+	"time"
+
 	sq "github.com/Masterminds/squirrel"
+	"github.com/giolekva/pcloud/core/kg/common"
 	"github.com/giolekva/pcloud/core/kg/model"
 	"github.com/giolekva/pcloud/core/kg/store"
+	"github.com/pkg/errors"
 )
 
 type SqlUserStore struct {
@@ -22,7 +27,7 @@ func newSqlUserStore(sqlStore *SqlStore) store.UserStore {
 
 	// note: we are providing field names explicitly here to maintain order of columns (needed when using raw queries)
 	us.usersQuery = us.getQueryBuilder().
-		Select("u.ID", "u.CreateAt", "u.UpdateAt", "u.DeleteAt", "u.Username", "u.Password", "u.Email", "u.EmailVerified", "u.FirstName", "u.LastName", "u.LastPasswordUpdate").
+		Select("u.ID", "u.CreateAt", "u.UpdateAt", "u.DeleteAt", "u.Username", "u.Password", "u.LastPasswordUpdate").
 		From("Users u")
 
 	schema := `CREATE TABLE IF NOT EXISTS Users (
@@ -39,25 +44,95 @@ func newSqlUserStore(sqlStore *SqlStore) store.UserStore {
 }
 
 func (us SqlUserStore) Save(user *model.User) (*model.User, error) {
-	return nil, nil
+	now := time.Now().Unix()
+
+	updatedUser := user.Clone()
+
+	if updatedUser.ID == "" {
+		updatedUser.ID = common.NewID()
+	}
+	updatedUser.CreateAt = now
+	updatedUser.UpdateAt = now
+	updatedUser.DeleteAt = 0
+	updatedUser.LastPasswordUpdate = now
+
+	query := us.getQueryBuilder().Insert("Users").
+		Columns("id", "username", "password", "create_at", "update_at", "delete_at", "last_password_update").
+		Values(updatedUser.ID, updatedUser.Username, updatedUser.Password, updatedUser.CreateAt, updatedUser.UpdateAt, updatedUser.DeleteAt, updatedUser.LastPasswordUpdate)
+
+	if _, err := query.Exec(); err != nil {
+		return nil, errors.Wrap(model.ErrInternal, err.Error())
+	}
+	return updatedUser, nil
 }
 
 func (us SqlUserStore) Get(id string) (*model.User, error) {
-	return nil, nil
+	return us.getUserByCondition(sq.Eq{"id": id})
 }
 
 func (us SqlUserStore) GetAll() ([]*model.User, error) {
-	return nil, nil
+	query := us.usersQuery.OrderBy("Username ASC")
+	rows, err := query.Query()
+	if err != nil {
+		return nil, errors.Wrap(model.ErrInternal, err.Error()) //TODO work on a clear db errors
+	}
+	return getUsersFromRows(rows)
 }
 
 func (us SqlUserStore) Count() (int64, error) {
-	return 0, nil
+	query := us.getQueryBuilder().
+		Select("count(*)").
+		From("Users").
+		Where(sq.Eq{"delete_at": 0})
+	row := query.QueryRow()
+
+	var count int64
+	err := row.Scan(&count)
+	if err != nil {
+		return 0, err
+	}
+
+	return count, nil
 }
 
 func (us SqlUserStore) GetAllWithOptions(page, perPage int) ([]*model.User, error) {
-	return nil, nil
+	query := us.usersQuery.Offset(uint64(page * perPage)).Limit(uint64(perPage)).OrderBy("Username ASC")
+	rows, err := query.Query()
+	if err != nil {
+		return nil, errors.Wrap(model.ErrInternal, err.Error()) //TODO work on a clear db errors
+	}
+	return getUsersFromRows(rows)
 }
 
 func (us SqlUserStore) GetByUsername(username string) (*model.User, error) {
-	return nil, nil
+	return us.getUserByCondition(sq.Eq{"username": username})
+}
+
+func (us SqlUserStore) getUserByCondition(condition sq.Eq) (*model.User, error) {
+	query := us.usersQuery.
+		Where(sq.Eq{"delete_at": 0}).
+		Where(condition)
+	row := query.QueryRow()
+	user := model.User{}
+
+	err := row.Scan(&user.ID, &user.Username, &user.Password, &user.CreateAt, &user.UpdateAt, &user.DeleteAt, &user.LastPasswordUpdate)
+	if err != nil {
+		return nil, errors.Wrap(err, "can't scan row")
+	}
+
+	return &user, nil
+}
+
+func getUsersFromRows(rows *sql.Rows) ([]*model.User, error) {
+	defer rows.Close()
+	results := []*model.User{}
+	for rows.Next() {
+		user := model.User{}
+		err := rows.Scan(&user.ID, &user.Username, &user.Password, &user.CreateAt, &user.UpdateAt, &user.DeleteAt, &user.LastPasswordUpdate)
+		if err != nil {
+			return nil, errors.Wrap(err, "can't scan user row")
+		}
+		results = append(results, &user)
+	}
+	return results, nil
 }
