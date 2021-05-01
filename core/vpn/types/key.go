d12813b75bc24650a02cb6f374223a9f4e1f1ab6
giolekva <giolekva@gmail.com>
2021-05-01 19:58:44 +0400

    Basic in memory implementation of network Manager

--- core/vpn/types/key.go
+++ core/vpn/types/key.go
@@ -1,6 +1,12 @@
 package types
 
-import "tailscale.com/types/key"
+import (
+	"encoding/hex"
+	"fmt"
+
+	"tailscale.com/control/controlclient"
+	"tailscale.com/types/key"
+)
 
 // Generates new private key.
 func NewPrivateKey() PrivateKey {
@@ -10,4 +16,9 @@ func NewPrivateKey() PrivateKey {
 // Returns public coutnerpart of the given private key.
 func (k PrivateKey) Public() PublicKey {
 	return PublicKey(key.Private(k).Public())
+}
+
+func (k DiscoKey) Endpoint() string {
+	discoHex := hex.EncodeToString(k[:])
+	return fmt.Sprintf("%s%s", discoHex, controlclient.EndpointDiscoSuffix)
 }
