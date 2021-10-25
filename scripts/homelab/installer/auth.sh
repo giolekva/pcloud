67fb42680f478fb37042a2510f2a6ed6daf005a5
giolekva <giolekva@gmail.com>
2021-10-25 20:42:07 +0400

    Auth: installer script

--- scripts/homelab/installer/auth.sh
+++ scripts/homelab/installer/auth.sh
@@ -1,25 +1,25 @@
 #!/bin/sh
 
-# helm install --namespace core-auth \
-#      postgres \
-#      bitnami/postgresql \
-#      --set fullnameOverride=postgres \
-#      --set image.repository=arm64v8/postgres \
-#      --set image.tag=13.4 \
-#      --set persistence.size=1Gi \
-#      --set securityContext.enabled=true \
-#      --set securityContext.fsGroup=0 \
-#      --set containerSecurityContext.enabled=true \
-#      --set containerSecurityContext.runAsUser=0 \
-#      --set volumePermissions.securityContext.runAsUser=0 \
-#      --set service.type=ClusterIP \
-#      --set service.port=5432 \
-#      --set postgresqlPassword=psswd \
-#      --set postgresqlDatabase=kratos
+helm install --namespace core-auth \
+     postgres \
+     bitnami/postgresql \
+     --set fullnameOverride=postgres \
+     --set image.repository=arm64v8/postgres \
+     --set image.tag=13.4 \
+     --set persistence.size=1Gi \
+     --set securityContext.enabled=true \
+     --set securityContext.fsGroup=0 \
+     --set containerSecurityContext.enabled=true \
+     --set containerSecurityContext.runAsUser=0 \
+     --set volumePermissions.securityContext.runAsUser=0 \
+     --set service.type=ClusterIP \
+     --set service.port=5432 \
+     --set postgresqlPassword=psswd \
+     --set postgresqlDatabase=kratos
 
-# kubectl create configmap kratos -n core-auth --from-file=../../core/auth/kratos.yaml
-# kubectl create configmap identity -n core-auth --from-file=../../core/auth/identity.schema.json
-# kubectl apply -f ../../core/auth/kratos/install.yaml
+kubectl create configmap kratos -n core-auth --from-file=../../core/auth/kratos/kratos.yaml
+kubectl create configmap identity -n core-auth --from-file=../../core/auth/kratos/identity.schema.json
+kubectl apply -f ../../core/auth/kratos/install.yaml
 
 kubectl create configmap \
 	-n core-auth \
