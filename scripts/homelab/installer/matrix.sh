5ca8b720fb527acc8a03e2165e5440343af1203a
giolekva <giolekva@gmail.com>
2021-10-27 14:58:37 +0400

    Matrix: SSO

--- scripts/homelab/installer/matrix.sh
+++ scripts/homelab/installer/matrix.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 kubectl apply -f ../../apps/matrix/install.yaml
-export KUBE_EDITOR="emacs -nw" && k edit configmap config -n app-matrix
+kubectl edit configmap config -n app-matrix
 helm install --create-namespace postgresql bitnami/postgresql \
      --namespace app-matrix \
      --set image.repository=arm64v8/postgres \
@@ -21,3 +21,20 @@ helm install --create-namespace postgresql bitnami/postgresql \
      --set initdbScripts."createdb\.sh"="createdb --encoding=UTF8 --locale=C --template=template0 --owner=synapse_user synapse"
 
 kubectl apply -f www.yaml
+
+
+## Integrate with PCloud OIDC Provider
+## TODO(giolekva): automate secret and config generation
+    # oidc_providers:
+    #   - idp_id: pcloud
+    #     idp_name: "PCloud OIDC Provider"
+    #     skip_verification: false
+    #     issuer: "https://hydra.lekva.me"
+    #     client_id: "matrix"
+    #     client_secret: ""
+    #     scopes: ["openid", "profile"]
+    #     allow_existing_users: true
+    #     user_mapping_provider:
+    #       config:
+    #         localpart_template: "{{ user.username }}"
+    #         display_name_template: "{{ user.username }}"
