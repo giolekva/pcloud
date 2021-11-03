4a021b196f7bf24b0f1634f68c85d0642b90ddc5
giolekva <giolekva@gmail.com>
2021-11-03 18:12:45 +0400

    VPN: point to app-pihole instead of pihole

--- scripts/homelab/installer/ingress-nginx.sh
+++ scripts/homelab/installer/ingress-nginx.sh
@@ -29,44 +29,44 @@
 # 	nodes-lighthouse-config \
 # 	--from-file installer/nodes-lighthouse.yaml
 
-kubectl apply -f installer/nodes-infrastructure.yaml
+# kubectl apply -f installer/nodes-infrastructure.yaml
 
 
 # kubectl apply -f installer/lighthouse-node.yaml
 
-# helm upgrade --create-namespace \
-#      --namespace ingress-nginx-private \
-#      nginx ingress-nginx/ingress-nginx \
-#      --version 4.0.3 \
-#      --set fullnameOverride=nginx-private \
-#      --set controller.service.type=ClusterIP \
-#      --set controller.ingressClassByName=true \
-#      --set controller.ingressClassResource.name=nginx-private \
-#      --set controller.ingressClassResource.enabled=true \
-#      --set controller.ingressClassResource.default=false \
-#      --set controller.ingressClassResource.controllerValue="k8s.io/ingress-nginx-private" \
-#      --set controller.extraVolumes[0].name="lighthouse-cert" \
-#      --set controller.extraVolumes[0].secret.secretName="node-lighthouse-cert" \
-#      --set controller.extraVolumes[1].name=config \
-#      --set controller.extraVolumes[1].configMap.name=lighthouse-config \
-#      --set controller.extraContainers[0].name=lighthouse \
-#      --set controller.extraContainers[0].image=giolekva/nebula:latest \
-#      --set controller.extraContainers[0].imagePullPolicy=IfNotPresent \
-#      --set controller.extraContainers[0].securityContext.capabilities.add[0]=NET_ADMIN \
-#      --set controller.extraContainers[0].securityContext.privileged=true \
-#      --set controller.extraContainers[0].ports[0].name=nebula \
-#      --set controller.extraContainers[0].ports[0].containerPort=4242 \
-#      --set controller.extraContainers[0].ports[0].protocol=UDP \
-#      --set controller.extraContainers[0].command[0]="nebula" \
-#      --set controller.extraContainers[0].command[1]="--config=/etc/nebula/config/lighthouse.yaml" \
-#      --set controller.extraContainers[0].volumeMounts[0].name=lighthouse-cert \
-#      --set controller.extraContainers[0].volumeMounts[0].mountPath=/etc/nebula/lighthouse \
-#      --set controller.extraContainers[0].volumeMounts[1].name=config \
-#      --set controller.extraContainers[0].volumeMounts[1].mountPath=/etc/nebula/config \
-#      --set controller.config.bind-address="111.0.0.1" \
-#      --set controller.config.proxy-body-size="0" \
-#      --set udp.53="pihole/pihole-dns-udp:53" \
-#      --set tcp.53="pihole/pihole-dns-tcp:53"
+helm upgrade --create-namespace \
+     --namespace ingress-nginx-private \
+     nginx ingress-nginx/ingress-nginx \
+     --version 4.0.3 \
+     --set fullnameOverride=nginx-private \
+     --set controller.service.type=ClusterIP \
+     --set controller.ingressClassByName=true \
+     --set controller.ingressClassResource.name=nginx-private \
+     --set controller.ingressClassResource.enabled=true \
+     --set controller.ingressClassResource.default=false \
+     --set controller.ingressClassResource.controllerValue="k8s.io/ingress-nginx-private" \
+     --set controller.extraVolumes[0].name="lighthouse-cert" \
+     --set controller.extraVolumes[0].secret.secretName="node-lighthouse-cert" \
+     --set controller.extraVolumes[1].name=config \
+     --set controller.extraVolumes[1].configMap.name=lighthouse-config \
+     --set controller.extraContainers[0].name=lighthouse \
+     --set controller.extraContainers[0].image=giolekva/nebula:latest \
+     --set controller.extraContainers[0].imagePullPolicy=IfNotPresent \
+     --set controller.extraContainers[0].securityContext.capabilities.add[0]=NET_ADMIN \
+     --set controller.extraContainers[0].securityContext.privileged=true \
+     --set controller.extraContainers[0].ports[0].name=nebula \
+     --set controller.extraContainers[0].ports[0].containerPort=4242 \
+     --set controller.extraContainers[0].ports[0].protocol=UDP \
+     --set controller.extraContainers[0].command[0]="nebula" \
+     --set controller.extraContainers[0].command[1]="--config=/etc/nebula/config/lighthouse.yaml" \
+     --set controller.extraContainers[0].volumeMounts[0].name=lighthouse-cert \
+     --set controller.extraContainers[0].volumeMounts[0].mountPath=/etc/nebula/lighthouse \
+     --set controller.extraContainers[0].volumeMounts[1].name=config \
+     --set controller.extraContainers[0].volumeMounts[1].mountPath=/etc/nebula/config \
+     --set controller.config.bind-address="111.0.0.1" \
+     --set controller.config.proxy-body-size="0" \
+     --set udp.53="app-pihole/pihole-dns-udp:53" \
+     --set tcp.53="app-pihole/pihole-dns-tcp:53"
 
      # # --set controller.extraVolumes[1].name=ca-cert \
      # # --set controller.extraVolumes[1].configMap.name=ca-cert \
