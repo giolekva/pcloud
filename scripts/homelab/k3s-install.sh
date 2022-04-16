d1a0528ff5ead8866eaec7f18e7a9894a6a22422
giolekva <giolekva@gmail.com>
2022-04-16 17:21:12 +0400

    refactor: deduplicate code in k3s-install

--- scripts/homelab/k3s-install.sh
+++ scripts/homelab/k3s-install.sh
@@ -1,36 +1,27 @@
 #!/bin/sh
 
+USER=pcloud
+
+K3S_VERSION="v1.23.5+k3s1"
+
+MASTER="192.168.0.111"
+WORKERS=("192.168.0.112" "192.168.0.113" "192.168.0.114" "192.168.0.116")
+
 k3sup install \
       --k3s-channel stable \
       --cluster \
-      --user pcloud \
-      --ip 192.168.0.111 \
+      --user $USER \
+      --ip $MASTER \
+      --k3s-version $K3S_VERSION \
       --k3s-extra-args "--node-taint pcloud=role:NoSchedule --disable traefik --disable local-storage --disable servicelb --kube-proxy-arg proxy-mode=ipvs --kube-proxy-arg ipvs-strict-arp --flannel-backend host-gw"
 
-k3sup join \
+for IP in "${WORKERS[@]}";
+do
+    k3sup join \
       --k3s-channel stable \
-      --ip 192.168.0.112 \
-      --user pcloud \
-      --server-user pcloud \
-      --server-ip 192.168.0.111
-
-k3sup join \
-      --k3s-channel stable \
-      --ip 192.168.0.113 \
-      --user pcloud \
-      --server-user pcloud \
-      --server-ip 192.168.0.111
-
-k3sup join \
-      --k3s-channel stable \
-      --ip 192.168.0.114 \
-      --user pcloud \
-      --server-user pcloud \
-      --server-ip 192.168.0.111
-
-k3sup join \
-      --k3s-channel stable \
-      --ip 192.168.0.116 \
-      --user pcloud \
-      --server-user pcloud \
-      --server-ip 192.168.0.111
+      --ip $IP \
+      --user $USER \
+      --server-user $USER \
+      --server-ip $MASTER \
+      --k3s-version $K3S_VERSION
+done
