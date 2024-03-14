ba589ae864f8a3afc6d897d966b0372433cce11c
Giorgi Lekveishvili <giolekva@gmail.com>
2024-03-14 18:51:34 +0400

    hetzner: automatically restart tailscale upon network setup (#95)
    Co-authored-by: Giorgi Lekveishvili <lekva@gl-mbp-m1-max.local>

--- scripts/hetzner/network.sh
+++ scripts/hetzner/network.sh
@@ -2,3 +2,6 @@
 
 ssh root@h01 "systemctl restart systemd-networkd"
 ssh root@h02 "systemctl restart systemd-networkd"
+
+ssh root@h01 "tailscale down"
+ssh root@h01 "tailscale up --advertise-routes=192.168.100.0/24"
