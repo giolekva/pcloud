9fe49184b20ae766397260e9cd2f5b60f717d716
Giorgi Lekveishvili <giolekva@gmail.com>
2024-03-26 10:54:07 +0400

    cleanup: remove old unused files (#122)
    Co-authored-by: Giorgi Lekveishvili <lekva@gl-mbp-m1-max.local>

--- scripts/overlaytest.sh
+++
@@ -1,3 +0,0 @@
-# kubectl apply -f ds-overlaytest.yaml
-
-echo "=> Start network overlay test"; kubectl get pods -l name=overlaytest -o jsonpath='{range .items[*]}{@.metadata.name}{" "}{@.spec.nodeName}{"\n"}{end}' | while read spod shost; do kubectl get pods -l name=overlaytest -o jsonpath='{range .items[*]}{@.status.podIP}{" "}{@.spec.nodeName}{"\n"}{end}' | while read tip thost; do kubectl --request-timeout='10s' exec $spod -- /bin/sh -c "ping -c2 $tip > /dev/null 2>&1"; RC=$?; if [ $RC -ne 0 ]; then echo $shost cannot reach $thost; fi; done; done; echo "=> End network overlay test"
